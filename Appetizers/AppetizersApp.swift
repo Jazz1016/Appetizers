e46f5129ea1767fe7fc1eb8a4e466da92bb42856
James Lea <james.q.lea@gmail.com>
2023-05-21 19:04:35 -0600

    first coomit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by James Lea on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
